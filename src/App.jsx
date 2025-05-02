11ab15961e4cb333a12e9c1ba72443e2c9c0ffc1
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-05-02 15:01:29 +0200

    Fetch data from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 // import { useState } from "react";
+import { useLaunchGame } from "./games.jsx";
 
 function App() {
+  const { games, loading } = useLaunchGame();
+
   return (
-    <>
-      <div>Game launcher</div>
-    </>
+    <div>
+      <h1>Game Launcher</h1>
+      {loading ? (
+        <p>Loading games...</p>
+      ) : (
+        <div>
+          {games.map((game, index) => (
+            <div key={index}>
+              <h2>{game.title}</h2>
+              <img
+                src={game.imageUrl}
+                alt={game.title}
+                style={{ width: "200px" }}
+              />
+              <p>
+                <strong>Description:</strong> {game.description}
+              </p>
+              <p>
+                <strong>Type:</strong> {game.gameType}
+              </p>
+              <p>
+                <strong>Long Description:</strong> {game.longDescription}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
---
+++ src/Games.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export function useLaunchGame() {
+  const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchGames() {
+      try {
+        const response = await fetch(
+          "https://game-launcher-1915c-default-rtdb.europe-west1.firebasedatabase.app/games.json"
+        );
+        const data = await response.json();
+        const gameList = Object.values(data); // ha object kulcsokkal jön
+        setGames(gameList);
+      } catch (error) {
+        console.error("Error fetching games:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchGames();
+  }, []);
+
+  return { games, loading };
+}
