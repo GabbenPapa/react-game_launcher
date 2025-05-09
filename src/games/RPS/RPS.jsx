4c8157b83c331e7f68819628b232ebcb53b72213
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-05-09 23:06:46 +0200

    RPS Refactor

--- src/games/RPS/RPS.jsx
+++ src/games/RPS/RPS.jsx
@@ -4,39 +4,34 @@ import paperImg from "../../assets/paper.png";
 import rockImg from "../../assets/rock.png";
 import scissorsImg from "../../assets/scissors.png";
 
-const rpsImages = {
-  1: paperImg,
-  2: rockImg,
-  3: scissorsImg,
-};
-
-const rpsNames = {
-  1: "Paper",
-  2: "Rock",
-  3: "Scissors",
-};
+const rpsOptions = [
+  { id: 1, name: "Paper", img: paperImg },
+  { id: 2, name: "Rock", img: rockImg },
+  { id: 3, name: "Scissors", img: scissorsImg },
+];
 
 export default function RPS() {
-  const [holded, setHolded] = useState(null);
+  const [computerMove, setComputerMove] = useState(null);
   const [winner, setWinner] = useState("");
 
-  function checkWinner(playerMove) {
-    const compMove = Math.trunc(Math.random() * 3) + 1;
-    setHolded(compMove);
+  const getWinner = (playerId, compId) => {
+    if (playerId === compId) return "It's a tie!";
 
-    if (playerMove === rpsNames[compMove]) {
-      setWinner("It's a tie!");
-    } else if (
-      (playerMove === "Rock" && compMove === 3) ||
-      (playerMove === "Paper" && compMove === 2) ||
-      (playerMove === "Scissors" && compMove === 1)
+    if (
+      (playerId === 2 && compId === 3) ||
+      (playerId === 1 && compId === 2) ||
+      (playerId === 3 && compId === 1)
     ) {
-      setWinner("You win!");
-    } else {
-      setWinner("Computer wins!");
+      return "You win!";
     }
-  }
+    return "Computer wins!";
+  };
 
+  const handleClick = (playerId) => {
+    const compId = Math.ceil(Math.random() * 3);
+    setComputerMove(compId);
+    setWinner(getWinner(playerId, compId));
+  };
   return (
     <div className="rps">
       <main>
@@ -44,21 +39,17 @@ export default function RPS() {
           <h1 className="rps-title">Rock Paper Scissors</h1>
           <h2 className="rps-subtitle">Choose your move</h2>
           <div className="player">
-            <div className="player-move" onClick={() => checkWinner("Rock")}>
-              <img src={rockImg} alt="Rock" className="player-img" />
-              <p className="player-text">Rock</p>
-            </div>
-            <div className="player-move" onClick={() => checkWinner("Paper")}>
-              <img src={paperImg} alt="Paper" className="player-img" />
-              <p className="player-text">Paper</p>
-            </div>
-
-            <div
-              className="player-move"
-              onClick={() => checkWinner("Scissors")}
-            >
-              <img src={scissorsImg} alt="Scissors" className="player-img" />
-              <p className="player-text">Scissors</p>
+            <div className="player">
+              {rpsOptions.map(({ id, name, img }) => (
+                <div
+                  key={id}
+                  className="player-move"
+                  onClick={() => handleClick(id)}
+                >
+                  <img src={img} alt={name} className="player-img" />
+                  <p className="player-text">{name}</p>
+                </div>
+              ))}
             </div>
           </div>
           {winner && (
@@ -67,17 +58,19 @@ export default function RPS() {
             </div>
           )}
         </div>
-        {holded && (
+        {computerMove && (
           <div className="rps-footer">
             <h2 className="rps-subtitle">Computer move</h2>
             <div className="player">
               <div className="computer-move">
                 <img
-                  src={rpsImages[holded]}
-                  alt={rpsNames[holded]}
+                  src={rpsOptions[computerMove - 1].img}
+                  alt={rpsOptions[computerMove - 1].name}
                   className="player-img"
                 />
-                <p className="player-text">{rpsNames[holded]}</p>
+                <p className="player-text">
+                  {rpsOptions[computerMove - 1].name}
+                </p>
               </div>
             </div>
           </div>
