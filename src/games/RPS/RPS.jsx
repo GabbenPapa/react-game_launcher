4e38289965a61eb8273340660117b3357cf49ed7
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-05-09 22:48:02 +0200

    Announce Winner

--- src/games/RPS/RPS.jsx
+++ src/games/RPS/RPS.jsx
@@ -18,9 +18,23 @@ const rpsNames = {
 
 export default function RPS() {
   const [holded, setHolded] = useState(null);
-  function handleHold() {
-    const holded = Math.trunc(Math.random() * 3) + 1;
-    setHolded(holded);
+  const [winner, setWinner] = useState("");
+
+  function checkWinner(playerMove) {
+    const compMove = Math.trunc(Math.random() * 3) + 1;
+    setHolded(compMove);
+
+    if (playerMove === rpsNames[compMove]) {
+      setWinner("It's a tie!");
+    } else if (
+      (playerMove === "Rock" && compMove === 3) ||
+      (playerMove === "Paper" && compMove === 2) ||
+      (playerMove === "Scissors" && compMove === 1)
+    ) {
+      setWinner("You win!");
+    } else {
+      setWinner("Computer wins!");
+    }
   }
 
   return (
@@ -28,27 +42,36 @@ export default function RPS() {
       <main>
         <div className="rps-header">
           <h1 className="rps-title">Rock Paper Scissors</h1>
-          <h2 className="rps-subtitle">Chouse your move</h2>
+          <h2 className="rps-subtitle">Choose your move</h2>
           <div className="player">
-            <div className="player-move" onClick={handleHold}>
+            <div className="player-move" onClick={() => checkWinner("Rock")}>
               <img src={rockImg} alt="Rock" className="player-img" />
               <p className="player-text">Rock</p>
             </div>
-            <div className="player-move" onClick={handleHold}>
+            <div className="player-move" onClick={() => checkWinner("Paper")}>
               <img src={paperImg} alt="Paper" className="player-img" />
               <p className="player-text">Paper</p>
             </div>
-            <div className="player-move" onClick={handleHold}>
+
+            <div
+              className="player-move"
+              onClick={() => checkWinner("Scissors")}
+            >
               <img src={scissorsImg} alt="Scissors" className="player-img" />
               <p className="player-text">Scissors</p>
             </div>
           </div>
+          {winner && (
+            <div className="rps-winner">
+              <h1 className="rps-title">{winner}</h1>
+            </div>
+          )}
         </div>
         {holded && (
           <div className="rps-footer">
             <h2 className="rps-subtitle">Computer move</h2>
             <div className="player">
-              <div className="player-move">
+              <div className="computer-move">
                 <img
                   src={rpsImages[holded]}
                   alt={rpsNames[holded]}
